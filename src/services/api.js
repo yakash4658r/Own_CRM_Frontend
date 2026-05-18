f27d07b6bc90b11e6ab278bb22e477a05fcf1867
yakash4658r <yakashelavarasan@outlook.com>
2026-05-18 23:16:50 +0530

    Update API baseURL to live Render server

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,25 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://own-crm.onrender.com/api',
 });
 
-api.interceptors.request.use((config) => {
-  const user = localStorage.getItem('user');
-  if (user) {
-    const parsedUser = JSON.parse(user);
-    if (parsedUser.token) {
-      config.headers.Authorization = `Bearer ${parsedUser.token}`;
+api.interceptors.request.use(
+  (config) => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      try {
+        const parsedUser = JSON.parse(user);
+        if (parsedUser.token) {
+          config.headers.Authorization = `Bearer ${parsedUser.token}`;
+        }
+      } catch (e) {
+        console.error('Failed to parse user from localStorage', e);
+      }
     }
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
